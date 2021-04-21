71e4c66be9174c6535ea2b8c22abbdbae38c73da
edsont8 <edsont8@hotmail.com>
2021-04-21 11:47:30 -0300

    added getch()

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>  //stdio = Standard Input Output
+#include <conio.h>  //importação necessaria para utilizar o getch();
 
 int main(){     //void ou int
 
     printf("Hello World!!!");  //printf = imprimir de forma formatada
+
+    getch();                   //getch() equivalente ao Console.ReadKey() do C#, 
+                                // que aguarda pressionar qualquer tecla para finalizar a execução.
 
     return 0;
 
